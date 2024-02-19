fc1db6a6f0e1761e7e71807c3d4b72ca25fc8bc5
Matiinglant <matiasdaniel1910@gmail.com>
2024-02-19 10:03:23 -0300

    Subo panel de administracion

--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
 from .forms import ProductoForm
 
@@ -19,3 +19,38 @@ def cargar_producto(request):
     else:
         form = ProductoForm()
     return render(request, 'productos/cargar_producto.html', {'form': form})
+
+#-------------------------------BUSCADOR----------------------------------------------------------
+
+#def search(request):
+#    return render(request, 'buscador.html')
+
+#def postSearchView(request):
+#    queryset = request.GET.get({% blocktrans %} buscar {% endblocktrans %})
+
+#    resultados = {}
+#    categorias = Categoria.objects.all()
+#    resultados['categorias'] = categorias
+
+#    if queryset:
+#        user = NewUser.objects.filter(username = queryset)
+#        resultados['posts'] = Post.objects.filter(
+#            Q(title__icontains = queryset) |
+#            Q(user__in = user)
+#        ).distinct()
+
+#    return render(request,'buscador.html', resultados)
+
+#def postCategoryView(request):
+
+#    categoria_id = request.GET.get('filtro', None)
+
+#    resultados = {}
+#    categorias = Categoria.objects.all()
+#    resultados['categorias'] = categorias
+
+#    if categoria_id:
+#        posteos = Post.objects.filter(categoria_id = categoria_id)
+#        resultados['posts'] = posteos 
+
+#    return render(request,'post/postCategory.html', resultados)
