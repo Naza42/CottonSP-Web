9ec069dba59da998950d1da71135975b65e945f0
Lucas Alfonsin <89589591+zLucas2021@users.noreply.github.com>
2024-02-19 16:09:44 -0300

    update producto model

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Producto(models.Model):
-    imagen = models.ImageField(upload_to='productos/')
+    imagen_principal = models.ImageField(upload_to='productos/',default='img/hero-banner.jpg',blank=False,null=False)
+    imagen_2 = models.ImageField(upload_to='productos/',blank=True,null=True)
+    imagen_3 = models.ImageField(upload_to='productos/',blank=True,null=True)
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    resumen = models.TextField()
+    descripcion = models.TextField(max_length = 256)
+    resumen = models.TextField(max_length = 256,blank = True)
+    resumen_2 = models.TextField(max_length = 256,blank = True)
 
     def __str__(self):
         return self.titulo
