9d7a269ed693322d80129ef0eb54675026f4b493
bloginfo-nuevo <120268810+sole1982@users.noreply.github.com>
2024-02-20 09:51:48 -0300

    Commit de mis cambios locales

--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Producto
 from .forms import ProductoForm
 
