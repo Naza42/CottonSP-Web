21a87c1995bf0f64d5ccfae3f8ccbf06ea76d6bd
Lucas Alfonsin <89589591+zLucas2021@users.noreply.github.com>
2024-03-04 13:55:01 -0300

    changed cards
    Se cambio el estilo de las cards y se mejoro el JS para su correcto funcionamiento

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,44 +1,56 @@
+const tarjetasWrapper = document.querySelector('.tarjetas-wrapper');
+let intervalo;
+let currentIndex = 0;
+let tarjetas = document.querySelectorAll('.test');
+let tarjetasVisibles = window.innerWidth <= 768 ? 1 : 3;
+
+function iniciarAvanceAutomatico() {
+  intervalo = setInterval(function() {
+    siguienteTarjeta();
+  }, 5000);
+}
+function reiniciarAvanceAutomatico() {
+  clearInterval(intervalo); 
+  iniciarAvanceAutomatico(); 
+}
+function mostrarTarjetas(index) {
+  const translateValue = -index * 100 + '%';
+  tarjetasWrapper.style.transform = 'translateX(' + translateValue + ')';
+}
+function siguienteTarjeta() {
+  const target = window.innerWidth <= 768 ? tarjetas.length - 1 : tarjetas.length%tarjetasVisibles;
+  const isNotEndCarousel =window.innerWidth <= 768 ? currentIndex < target : currentIndex <= target;
+  if (isNotEndCarousel) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  mostrarTarjetas(currentIndex);
+}
+function anteriorTarjeta() {
+  const target = window.innerWidth <= 768 ? tarjetas.length - tarjetasVisibles : tarjetas.length%tarjetasVisibles  + 1;
+  if (currentIndex != 0) {
+    currentIndex--;
+  } else {
+    currentIndex = target;
+  }
+  mostrarTarjetas(currentIndex);
+}
+
 document.querySelector('#tarjetasCarrusel').addEventListener('click', function(event) {
-  console.log('Evento de clic detectado');
-  // Puedes mantener el código existente para manejar el clic de los botones
+  tarjetas = document.querySelectorAll('.test');
+  reiniciarAvanceAutomatico();
   if (event.target.classList.contains('siguiente')) {
     siguienteTarjeta();
   } else if (event.target.classList.contains('anterior')) {
     anteriorTarjeta();
   }
 });
+window.addEventListener('resize', function() {
+  currentIndex = 0
+  tarjetasVisibles = window.innerWidth <= 768 ? 1 : 3;
+  mostrarTarjetas(currentIndex)
+  reiniciarAvanceAutomatico();
+});
 
- // Iniciar el avance automático cada 1 minuto
- setInterval(function() {
-  console.log("Start")
-  siguienteTarjeta();
-  }, 15000);  // 60000 milisegundos = 1 minuto
-
-let currentIndex = 0;
-const tarjetasWrapper = document.querySelector('.tarjetas-wrapper');
-const tarjetas = document.querySelectorAll('.tarjeta');
-
-function mostrarTarjetas(index) {
-const translateValue = -index * 100 + '%';
-tarjetasWrapper.style.transform = 'translateX(' + translateValue + ')';
-}
-
-function siguienteTarjeta() {
-const tarjetasVisibles = window.innerWidth <= 768 ? 11 : 3; // Ajusta según tus necesidades
-if (currentIndex < tarjetasVisibles) {
-  currentIndex++;
-} else {
-  currentIndex = 0;
-}
-mostrarTarjetas(currentIndex);
-}
-
-function anteriorTarjeta() {
-const tarjetasVisibles = window.innerWidth <= 768 ? 1 : 3; // Ajusta según tus necesidades
-if (currentIndex > 0) {
-  currentIndex--;
-} else {
-  currentIndex = tarjetasVisibles;
-}
-mostrarTarjetas(currentIndex);
-}
+iniciarAvanceAutomatico();
